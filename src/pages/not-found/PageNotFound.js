e3ebf60579b6585b68cf7bbc15ebf8aad6e79952
fstipo <fstipanov@gmail.com>
2022-09-29 12:04:24 +0200

    page-404 border radius

--- src/pages/not-found/PageNotFound.js
+++ src/pages/not-found/PageNotFound.js
@@ -3,7 +3,7 @@ import "./PageNotFound.css"
 
 const PageNotFound = () => {
     return (
-        <div>
+        <div className="page-404-container">
             <h1 className='header-title'>404</h1>
             <div className='cloak__wrapper'>
                 <div className='cloak__container'>
